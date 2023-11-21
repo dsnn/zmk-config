c18eddffd5f87035bbba53f5789f634b194066dc
dsn <dsn@dsnn.io>
2023-11-21 16:58:21 +0100

    debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
-west build -d build/left -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-config/config/ -DSHIELD=corne_left nice_view nice_view_adapter
-west build -d build/right -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-config/config/ -DSHIELD=corne_right nice_view nice_view_adapter
+west build -d build/left -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-config/config/ -DSHIELD="corne_left nice_view_adapter nice_view"
+west build -d build/right -b nice_nano_v2 -- -DZMK_CONFIG=../../zmk-config/config/ -DSHIELD="corne_right nice_view_adapter nice_view"
 echo 'cleanup'
 rm /workspace/zmk-config/out/left.uf2
 rm /workspace/zmk-config/out/right.uf2
